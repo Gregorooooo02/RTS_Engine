c25b51398851bacc2265020446256e23e21beaf7
Grzegorz Bednarek <gregorooooo02@gmail.com>
2024-03-20 10:47:43 +0100

    Rendering a cube

--- Game1.cs
+++ Game1.cs
@@ -10,6 +10,16 @@ public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+
+    private Model _model;
+    
+    private Matrix _world = Matrix.CreateTranslation(new Vector3(0.0f, 0.0f, 0.0f));
+    private Matrix _view = Matrix.CreateLookAt(
+        new Vector3(0.0f, 0.0f, 10.0f),
+        new Vector3(0.0f),
+        -Vector3.UnitY);
+    private Matrix _projection =
+        Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), 1440.0f / 900.0f, 0.1f, 100.0f); 
     
     private ImGuiRenderer _imGuiRenderer;
     private Num.Vector3 _clearColor = new Num.Vector3(0.0f, 0.0f, 0.0f);
@@ -37,6 +47,8 @@ public class Game1 : Game
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
+        
+        _model = Content.Load<Model>("cube");
 
         // TODO: use this.Content to load your game content here
     }
@@ -54,13 +66,29 @@ public class Game1 : Game
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(new Color(_clearColor));
-
+        
+        DrawModel(_model, _world, _view, _projection);
+        
         // TODO: Add your drawing code here
         _imGuiRenderer.BeforeLayout(gameTime);
         ImGuiLayout();
         _imGuiRenderer.AfterLayout();
 
         base.Draw(gameTime);
+    }
+
+    private void DrawModel(Model model, Matrix wrld, Matrix vw, Matrix proj)
+    {
+        foreach (ModelMesh mesh in model.Meshes)
+        {
+            foreach (BasicEffect effect in mesh.Effects)
+            {
+                effect.World = wrld;
+                effect.View = vw;
+                effect.Projection = proj;
+            }
+            mesh.Draw();
+        }
     }
 
     protected virtual void ImGuiLayout()
