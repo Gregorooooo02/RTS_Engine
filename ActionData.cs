b2670c8ad951cfe6776bc1aaff3c9c54a17befe3
242500 <126494791+bazyl02@users.noreply.github.com>
2024-03-22 19:35:48 +0100

    Added keyboard input handling

---
+++ ActionData.cs
@@ -0,0 +1,33 @@
+﻿namespace RTS_Engine;
+
+public enum ActionState
+{
+    PRESSED,
+    RELEASED
+}
+
+public class ActionData
+{
+    public ActionState state;
+    public int duration;
+    public GameAction action;
+
+    public ActionData(GameAction action)
+    {
+        duration = 0;
+        state = ActionState.PRESSED;
+        this.action = action;
+    }
+
+    public void UpdateAction(bool isPressed = true)
+    {
+        if (isPressed)
+        {
+            duration++;
+        }
+        else
+        {
+            state = ActionState.RELEASED;
+        }
+    }
+}
---
+++ InputManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework.Input;
+
+namespace RTS_Engine;
+
+public enum GameAction
+{
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT,
+    EXIT
+}
+
+public class InputManager
+{
+    //Singleton part
+    public static InputManager Instance;
+    public static void Initialize()
+    {
+        Instance = new InputManager();
+    }
+    private InputManager()
+    {
+        RawToAction = new Dictionary<Keys, GameAction>();
+        Actions = new List<ActionData>();
+        PopulateDict();
+    }
+    
+    //Other
+    private List<ActionData> Actions;
+    private Dictionary<Keys, GameAction> RawToAction;
+
+    public void PopulateDict()
+    {
+        //open config file here and fill dictionary with it
+        RawToAction.Clear();
+        RawToAction.Add(Keys.W,GameAction.FORWARD);
+        RawToAction.Add(Keys.S,GameAction.BACKWARD);
+        RawToAction.Add(Keys.A,GameAction.LEFT);
+        RawToAction.Add(Keys.D,GameAction.RIGHT);
+        RawToAction.Add(Keys.Escape,GameAction.EXIT);
+    }
+
+    public void PollInput()
+    {
+        //Poll current frame inputs
+        Keys[] input = Keyboard.GetState().GetPressedKeys();
+        List<GameAction> currentActions = new List<GameAction>();
+        foreach (Keys k in input)
+        {
+            if (RawToAction.TryGetValue(k, out GameAction action))
+            {
+                ActionData a = Actions.Find(x => x.action == action);
+                if (a != null)
+                {
+                    a.UpdateAction();
+                }
+                else
+                {
+                    Actions.Add(new ActionData(action));
+                }
+                currentActions.Add(action);
+            }
+        }
+
+        //Update no longer active actions
+        for (int i = Actions.Count - 1;i >= 0;i--)
+        {
+            ActionData a = Actions[i];
+            if (!currentActions.Contains(a.action))
+            {
+                if (a.state == ActionState.PRESSED)
+                {
+                    a.UpdateAction(false);
+                }
+                else
+                {
+                    Actions.Remove(a);
+                }
+            }
+        }
+    }
+
+    public bool IsActive(GameAction action)
+    {
+        return GetAction(action) != null;
+    }
+
+    public ActionData GetAction(GameAction action)
+    {
+        return Actions.Find(x => x.action == action);
+    }
+}
