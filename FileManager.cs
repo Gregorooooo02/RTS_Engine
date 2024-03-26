e659a8021c52b6363bf9bfc4fb337c7166de0651
Grzegorz Bednarek <gregorooooo02@gmail.com>
2024-03-26 22:49:07 +0100

    Created basic XML file with keybindings - no errors so far

--- FileManager.cs
+++ FileManager.cs
@@ -1,6 +1,32 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
 namespace RTS_Engine;
 
 public class FileManager
 {
+    public static FileManager Instance;
+    public static void Initialize()
+    {
+        Instance = new FileManager();
+    }
     
+    public Dictionary<String, String> ReadConfigFile(String path)
+    {
+        Dictionary<String, String> dict = new Dictionary<String, String>();
+        
+        // Read file and fill dictionary with it
+        dict.Add("Forward", "W");
+        dict.Add("Backward", "S");
+        dict.Add("Left", "A");
+        dict.Add("Right", "D");
+        dict.Add("Exit", "Escape");
+        dict.Add("LMB", "0");
+        dict.Add("MMB", "1");
+        dict.Add("RMB", "2");
+        
+        return dict;
+    }
 }
