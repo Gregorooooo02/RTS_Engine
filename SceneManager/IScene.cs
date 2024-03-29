d0c23a35b8d2f2942474fa624248d0ddb4a74581
Grzegorz Bednarek <gregorooooo02@gmail.com>
2024-03-29 01:44:36 +0100

    Implementing SceneManager
    - [Added] IScene interface to create multiple classes of scenes
    - [Added] SceneManager class to manage the scenes in the game
    
    [Changed] ImGui.ini parameters

---
+++ SceneManager/IScene.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace RTS_Engine;
+
+public interface IScene
+{
+    public void Load();
+    public void Update(GameTime gameTime);
+    public void Draw();
+}
---
+++ SceneManager/SceneManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace RTS_Engine;
+
+public class SceneManager
+{
+    private readonly Stack<IScene> _scenes;
+
+    public SceneManager()
+    {
+        _scenes = new Stack<IScene>();
+    }
+
+    public void AddScene(IScene scene) 
+    {
+        _scenes.Push(scene);
+    }
+
+    public void RemoveScene() 
+    {
+        _scenes.Pop();
+    }
+
+    public IScene GetCurrentScene() 
+    {
+        return _scenes.Peek();
+    }
+}
