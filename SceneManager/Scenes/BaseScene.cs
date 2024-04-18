841e5aa0a255c585494ef94be8f06aeb5ef837d5
Grzegorz Bednarek <gregorooooo02@gmail.com>
2024-04-18 21:49:54 +0200

    Adding new models into the Content; deleting everything from the BaseScene

--- SceneManager/Scenes/BaseScene.cs
+++ SceneManager/Scenes/BaseScene.cs
@@ -13,41 +13,11 @@ public class BaseScene : Scene
     {
         Name = "BaseScene";
         SceneRoot = new GameObject();
-        
-        GameObject background = new GameObject();
-        background.AddComponent<AnimatedSpriteRenderer>();
-        background.GetComponent<AnimatedSpriteRenderer>().SetSpriteSheet(AssetManager.Background);
-        background.GetComponent<AnimatedSpriteRenderer>().SetFrames(12);
-        background.GetComponent<AnimatedSpriteRenderer>().SetFrameTime(0.05f);
-        background.Transform.SetLocalScale(new Vector3(1.0f, 10.0f, 1.0f));
-        background.Transform.SetLocalPosition(new Vector3(0, 0, 0));
-        SceneRoot.AddChildObject(background);
-
-        GameObject coin = new GameObject();
-        coin.AddComponent<AnimatedSpriteRenderer>();
-        coin.GetComponent<AnimatedSpriteRenderer>().SetSpriteSheet(AssetManager.DefaultAnimatedSprite);
-        coin.GetComponent<AnimatedSpriteRenderer>().SetFrames(6);
-        coin.GetComponent<AnimatedSpriteRenderer>().SetFrameTime(0.1f);
-        coin.Transform.SetLocalPosition(new Vector3(680, 450, 0));
-        coin.Transform.SetLocalScale(new Vector3(0.45f, 3.0f, 1.0f));
-        SceneRoot.AddChildObject(coin);
-
-        GameObject smiley = new GameObject();
-        smiley.AddComponent<SpiteRenderer>();
-        smiley.Transform.SetLocalScale(new Vector3(0.25f, 0.25f, 0.25f));
-        SceneRoot.AddChildObject(smiley);
-
-        GameObject text = new GameObject();
-        text.AddComponent<TextRenderer>();
-        text.Transform.SetLocalPosition(new Vector3(15, 800, 0));
-        SceneRoot.AddChildObject(text);
     }
 
     public override void Update(GameTime gameTime)
     {
-        SceneRoot.Children[3].GetComponent<TextRenderer>().NewContent = "Number count: " + number;
         SceneRoot.Update();
-        number++;
     }
 
     public override void Draw(Matrix _view, Matrix _projection)
