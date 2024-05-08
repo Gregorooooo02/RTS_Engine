8fa614b5e61c2f91163278477548b48fc47c87f8
Grzegorz Bednarek <gregorooooo02@gmail.com>
2024-05-08 22:46:45 +0200

    Starting implementing Animation Component - starting with bones and clip
    [Added] AnimationClip Class:
    
    - [Added] Two nested classes: Keyframe and Bone, because an animation clip is just a set of keyframes with associated bones
    - [Added] Time, Rotation, Translation and Transform properties in Keyframe Class
    - [Added] Name, List of keyframes properties in the Bone Class
    - [Added] List of bones, name and the duration in the Keyframe Class
    
    [Added] Bone Class:
    
    - [Added] Basic hierarchy system of the bones
    - [Added] Transformations matrix and vectors for scale and translation, and a quaternion for the rotation
    - [Added] Same properties for the fields
    
    [Added] AnimationClipReader Class:
    
    - [Added] Inheritance from the ContentTypeReader<T> which can help reading the AnimationClips from the Content Pipeline - that's nice

---
+++ SkinnedAnimationAux/AnimationClip.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace RTS_Engine;
+
+public class AnimationClip
+{
+    public class Keyframe
+    {
+        public double Time;
+        public Quaternion Rotation;
+        public Vector3 Translation;
+
+        public Matrix Transform
+        {
+            get => Matrix.CreateFromQuaternion(Rotation) * Matrix.CreateTranslation(Translation);
+            set 
+            {
+                Matrix transform = value;
+                transform.Right = Vector3.Normalize(transform.Right);
+                transform.Up = Vector3.Normalize(transform.Up);
+                transform.Backward = Vector3.Normalize(transform.Backward);
+
+                Rotation = Quaternion.CreateFromRotationMatrix(transform);
+                Translation = transform.Translation;
+            }
+        }
+    }
+
+    public class Bone
+    {
+        private string name = "";
+        private List<Keyframe> ketyframes = new List<Keyframe>();
+        public string Name { get => name; set => name = value; }
+        public List<Keyframe> Keyframes { get => ketyframes; }
+    }
+
+    private List<Bone> bones = new List<Bone>();
+    public string Name;
+    public double Duration;
+    public List<Bone> Bones { get => bones; }
+}
---
+++ SkinnedAnimationAux/Bone.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace RTS_Engine;
+
+public class Bone
+{
+#region Fields
+    private Bone parent = null;
+    private List<Bone> children = new List<Bone>();
+    private Matrix bindTransform = Matrix.Identity;
+    private Vector3 bindScale = Vector3.One;
+    private Vector3 translation = Vector3.Zero;
+    private Quaternion rotation = Quaternion.Identity;
+    private Vector3 scale = Vector3.One;
+#endregion
+    public string Name = "";
+    public Matrix BindTransform { get => bindTransform; }
+    public Matrix SkinTransform { get; set; }
+    public Quaternion Rotation { get => rotation; set => rotation = value; }
+    public Vector3 Translation { get => translation; set => translation = value; }
+    public Vector3 Scale { get => scale; set => scale = value; }
+    public Bone Parent { get => parent; }
+    public List<Bone> Children { get => children; }
+    public Matrix AbsoluteTransform = Matrix.Identity;
+#region Methods
+    public Bone(string name, Matrix bindTransform, Bone parent)
+    {
+        this.Name = name;
+        this.parent = parent;
+
+        if (parent != null)
+        {
+            parent.children.Add(this);
+        }
+
+        this.bindScale = new Vector3(
+            bindTransform.Right.Length(),
+            bindTransform.Up.Length(),
+            bindTransform.Backward.Length()
+        );
+
+        bindTransform.Right = bindTransform.Right / bindScale.X;
+        bindTransform.Up = bindTransform.Up / bindScale.Y;
+        bindTransform.Backward = bindTransform.Backward / bindScale.Z;
+        this.bindTransform = bindTransform;
+        
+        ComputeAbsoluteTransform();
+        SkinTransform = Matrix.Invert(AbsoluteTransform);
+    }
+
+    public void ComputeAbsoluteTransform()
+    {
+        Matrix transform = Matrix.CreateScale(Scale * bindScale) *
+                           Matrix.CreateFromQuaternion(Rotation) *
+                           Matrix.CreateTranslation(Translation) *
+                           bindTransform;
+
+        if (parent != null)
+        {
+            AbsoluteTransform = transform * parent.AbsoluteTransform;
+        }
+        else
+        {
+            AbsoluteTransform = transform;
+        }
+    }
+
+    public void SetCompleteTransform(Matrix m)
+    {
+        Matrix setTo = m * Matrix.Invert(BindTransform);
+
+        Translation = setTo.Translation;
+        Rotation = Quaternion.CreateFromRotationMatrix(setTo);
+    }
+#endregion
+}
