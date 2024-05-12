4e7797e40f034362045a3d9eec2deb36e2d6d63a
Grzegorz Bednarek <gregorooooo02@gmail.com>
2024-05-12 18:03:39 +0200

    Almost finished AnimationPipeline - just need to add a drawing method for the Game Object
    [Changed] SkinnedAnimationAux directory is now named AnimationPipeline
    
    [Added] AnimationProcessor Class for the logic of the played animation
    
    - [Added] Process() and ProcessSkeleton() method to process the loaded animation and the rig of the model
    
    [Added] AnimationPlayer Class, which is responsible for playing the animation
    
    - [Added] Update() method to get the duration of the animation
    - [Added] Internal BoneInfo Class, to get the info about the changing rig bones in the model
    
    [Added] AnimatedModelRenderer Class - will be a component in the future
    
    - [Added] LoadContent() method to load up the necessary properties for the model
    - [Added] PlayClip() method to play the animation
    - [Added] Update() method to update the model in the runtime
    - [Added] Draw() method - not yet implemented

---
+++ AnimationPipeline/AnimationProcessor.cs
@@ -0,0 +1,389 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content.Pipeline;
+using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
+using Microsoft.Xna.Framework.Content.Pipeline.Processors;
+
+namespace RTS_Engine;
+
+public class AnimationProcessor : ModelProcessor 
+{
+    private ModelContent _model;
+    private ModelProperties _modelProperties = new ModelProperties();
+
+    private Dictionary<MaterialContent, SkinnedMaterialContent> _toSkinnedMaterial = new Dictionary<MaterialContent, SkinnedMaterialContent>();
+
+    public override ModelContent Process(NodeContent input, ContentProcessorContext context)
+    {
+        BoneContent skeleton = ProcessSkeleton(input);
+        SwapSkinnedMaterial(input);
+        _model = base.Process(input, context);
+
+        ProcessAnimations(_model, input, context);
+
+        return _model;
+    }
+
+    private BoneContent ProcessSkeleton(NodeContent input)
+    {
+        BoneContent skeleton = MeshHelper.FindSkeleton(input);
+        if (skeleton == null)
+            throw new InvalidContentException("Input skeleton not found.");
+        
+        FlattenTransforms(input, skeleton);
+
+        TrimSkeleton(skeleton);
+
+        List<NodeContent> nodes = FlattenHierarchy(input);
+        IList<BoneContent> bones = MeshHelper.FlattenSkeleton(skeleton);
+
+        Dictionary<NodeContent, int> nodeToIndex = new Dictionary<NodeContent, int>();
+        for (int i = 0; i < nodes.Count; i++)
+            nodeToIndex.Add(nodes[i], i);
+        
+        foreach (BoneContent bone in bones)
+        {
+            _modelProperties.Skeleton.Add(nodeToIndex[bone]);
+        }
+
+        return skeleton;
+    }
+
+    private List<NodeContent> FlattenHierarchy(NodeContent item)
+    {
+        List<NodeContent> nodes = new List<NodeContent>();
+        nodes.Add(item);
+
+        foreach (NodeContent child in item.Children)
+        {
+            FlattenHierarchy(nodes, child);
+        }
+
+        return nodes;
+    }
+
+    private void FlattenHierarchy(List<NodeContent> nodes, NodeContent item)
+    {
+        nodes.Add(item);
+
+        foreach (NodeContent child in item.Children)
+        {
+            FlattenHierarchy(nodes, child);
+        }
+    }
+
+    private void FlattenTransforms(NodeContent node, BoneContent skeleton)
+    {
+        foreach (NodeContent child in node.Children)
+        {
+            if (child == skeleton)
+            {
+                continue;
+            }
+
+            if (IsSkinned(child))
+            {
+                FlattenAllTransforms(child);
+            }
+        }
+    }
+
+    private void FlattenAllTransforms(NodeContent node)
+    {
+        MeshHelper.TransformScene(node, node.Transform);
+
+        node.Transform = Matrix.Identity;
+
+        foreach (NodeContent child in node.Children)
+        {
+            FlattenAllTransforms(child);
+        }
+    }
+
+    private void TrimSkeleton(NodeContent skeleton)
+    {
+        List<NodeContent> toRemove = new List<NodeContent>();
+
+        foreach (NodeContent child in skeleton.Children)
+        {
+            if (child.Name.EndsWith("Nub") || child.Name.EndsWith("Footsteps"))
+            {
+                toRemove.Add(child);
+            }
+            else
+            {
+                TrimSkeleton(child);
+            }
+        }
+
+        foreach (NodeContent child in toRemove)
+        {
+            skeleton.Children.Remove(child);
+        }
+    }
+
+    private bool IsSkinned(NodeContent node)
+    {
+        MeshContent mesh = node as MeshContent;
+
+        if (mesh != null)
+        {
+            foreach (GeometryContent geometry in mesh.Geometry)
+            {
+                foreach (VertexChannel vchannel in geometry.Vertices.Channels)
+                {
+                    if (vchannel is VertexChannel<BoneWeightCollection>)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private void SwapSkinnedMaterial(NodeContent node)
+    {
+        MeshContent mesh = node as MeshContent;
+
+        if (mesh != null)
+        {
+            foreach (GeometryContent geometry in mesh.Geometry)
+            {
+                bool swap = false;
+                foreach (VertexChannel vchannel in geometry.Vertices.Channels)
+                {
+                    if (vchannel is VertexChannel<BoneWeightCollection>)
+                    {
+                        swap = true;
+                        break;
+                    }
+                }
+
+                if (swap)
+                {
+                    if (_toSkinnedMaterial.ContainsKey(geometry.Material)) 
+                    {
+                        geometry.Material = _toSkinnedMaterial[geometry.Material];
+                    }
+                    else
+                    {
+                        SkinnedMaterialContent smaterial = new SkinnedMaterialContent();
+                        BasicMaterialContent bmaterial = geometry.Material as BasicMaterialContent;
+
+                        smaterial.Alpha = bmaterial.Alpha;
+                        smaterial.DiffuseColor = bmaterial.DiffuseColor;
+                        smaterial.EmissiveColor = bmaterial.EmissiveColor;
+                        smaterial.SpecularColor = bmaterial.SpecularColor;
+                        smaterial.SpecularPower = bmaterial.SpecularPower;
+                        smaterial.Texture = bmaterial.Texture;
+                        smaterial.WeightsPerVertex = 4;
+
+                        _toSkinnedMaterial[geometry.Material] = smaterial;
+                        geometry.Material = smaterial;
+                    }
+                }
+            }
+        }
+
+        foreach (NodeContent child in node.Children)
+        {
+            SwapSkinnedMaterial(child);
+        }
+    }
+
+#region Animation Support
+
+    private Dictionary<string, int> _bones = new Dictionary<string, int>();
+    private Matrix[] _boneTransforms;
+    private Dictionary<string, AnimationClip> _clips = new Dictionary<string, AnimationClip>();
+
+    private void ProcessAnimations(ModelContent model, NodeContent input, ContentProcessorContext context)
+    {
+        for (int i = 0 ; i < model.Bones.Count; i++)
+        {
+            _bones[model.Bones[i].Name] = i;
+        }
+
+        _boneTransforms = new Matrix[model.Bones.Count];
+
+        ProcessAnimationRecursive(input);
+
+        if (_modelProperties.AnimationClips.Count == 0)
+        {
+            AnimationClip clip = new AnimationClip();
+            _modelProperties.AnimationClips.Add(clip);
+
+            string clipName = "Take 001";
+
+            _clips[clipName] = clip;
+            clip.Name = clipName;
+
+            foreach (ModelBoneContent bone in model.Bones)
+            {
+                AnimationClip.Bone clipBone = new AnimationClip.Bone();
+                clipBone.Name = bone.Name;
+
+                clip.Bones.Add(clipBone);
+            }
+        }
+
+        foreach (AnimationClip clip in _modelProperties.AnimationClips)
+        {
+            for (int b = 0; b < _bones.Count; b++)
+            {
+                List<AnimationClip.Keyframe> keyframes = clip.Bones[b].Keyframes;
+
+                if (keyframes.Count == 0 || keyframes[0].Time > 0)
+                {
+                    AnimationClip.Keyframe keyframe = new AnimationClip.Keyframe();
+                    keyframe.Time = 0;
+                    keyframe.Transform = _boneTransforms[b];
+                    keyframes.Insert(0, keyframe);
+                }
+            }
+        }
+    }
+
+    private void ProcessAnimationRecursive(NodeContent input)
+    {
+        int inputBoneIndex;
+
+        if (_bones.TryGetValue(input.Name, out inputBoneIndex))
+        {
+            _boneTransforms[inputBoneIndex] = input.Transform;
+        }
+
+        foreach (KeyValuePair<string, AnimationContent> animation in input.Animations)
+        {
+            AnimationClip clip;
+            string clipName = animation.Key;
+
+            if (!_clips.TryGetValue(clipName, out clip))
+            {
+                clip = new AnimationClip();
+                _modelProperties.AnimationClips.Add(clip);
+                _clips[clipName] = clip;
+
+                clip.Name = clipName;
+
+                foreach (ModelBoneContent bone in _model.Bones)
+                {
+                    AnimationClip.Bone clipBone = new AnimationClip.Bone();
+                    clipBone.Name = bone.Name;
+
+                    clip.Bones.Add(clipBone);
+                }
+            }
+
+            if (animation.Value.Duration.TotalSeconds > clip.Duration)
+            {
+                clip.Duration = animation.Value.Duration.TotalSeconds;
+            }
+
+            foreach (KeyValuePair<string, AnimationChannel> channel in animation.Value.Channels)
+            {
+                int boneIndex;
+
+                if (!_bones.TryGetValue(channel.Key, out boneIndex))
+                {
+                    continue;
+                }
+
+                if (UselessAnimationTest(boneIndex))
+                {
+                    continue;
+                }
+
+                LinkedList<AnimationClip.Keyframe> keyframes = new LinkedList<AnimationClip.Keyframe>();
+
+                foreach (AnimationKeyframe keyframe in channel.Value)
+                {
+                    Matrix transform = keyframe.Transform;
+
+                    AnimationClip.Keyframe newKeyframe = new AnimationClip.Keyframe();
+                    newKeyframe.Time = keyframe.Time.TotalSeconds;
+                    newKeyframe.Transform = transform;
+
+                    keyframes.AddLast(newKeyframe);
+                }
+
+                LinearKeyframeReduction(keyframes);
+
+                foreach (AnimationClip.Keyframe keyframe in keyframes)
+                {
+                    clip.Bones[boneIndex].Keyframes.Add(keyframe);
+                }
+            }
+        }
+
+        foreach (NodeContent child in input.Children)
+        {
+            ProcessAnimationRecursive(child);
+        }
+    }
+
+    private const float TinyLength = 1e-8f;
+    private const float TinyCosAngle = 0.9999999f;
+
+    private void LinearKeyframeReduction(LinkedList<AnimationClip.Keyframe> keyframes)
+    {
+        if (keyframes.Count < 3)
+        {
+            return;
+        }
+
+        for (LinkedListNode<AnimationClip.Keyframe> node = keyframes.First.Next; ;)
+        {
+            LinkedListNode<AnimationClip.Keyframe> next = node.Next;
+
+            if (next == null)
+            {
+                break;
+            }
+
+            AnimationClip.Keyframe a = node.Previous.Value;
+            AnimationClip.Keyframe b = node.Value;
+            AnimationClip.Keyframe c = next.Value;
+
+            float t = (float)((node.Value.Time - node.Previous.Value.Time) /
+                (next.Value.Time - node.Previous.Value.Time));
+
+            Vector3 translation = Vector3.Lerp(a.Translation, c.Translation, t);
+            Quaternion rotation = Quaternion.Slerp(a.Rotation, c.Rotation, t);
+
+            if ((translation - b.Translation).LengthSquared() < TinyLength &&
+                Quaternion.Dot(rotation, b.Rotation) > TinyCosAngle)
+            {
+                keyframes.Remove(node);
+            }
+
+            node = next;
+        }
+    }
+
+    private bool UselessAnimationTest(int boneIndex)
+    {
+        foreach (ModelMeshContent mesh in _model.Meshes)
+        {
+            if (mesh.ParentBone.Index == boneIndex)
+            {
+                return false;
+            }
+        }
+
+        foreach (int b in _modelProperties.Skeleton)
+        {
+            if (b == boneIndex)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+#endregion
+}
---
+++ Components/AnimatedModelRenderer.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace RTS_Engine;
+
+public class AnimatedModelRenderer
+{
+#region Fields
+    private Model _model = null;
+    private ModelProperties _modelProperties = null;
+    private List<Bone> _bones = new List<Bone>();
+    private Matrix[] _skeleton;
+    private Matrix[] _boneTransforms;
+
+    private string _assetName = "";
+    private AnimationPlayer _animationPlayer = null;
+
+    private bool _hasSkinnedVertexType = false;
+    private bool _hasNormals = false;
+    private bool _hasTexCoords = false;
+#endregion
+
+#region Properties
+    public Model Model { get => _model; }
+    public List<Bone> Bones { get => _bones; }
+    public List<AnimationClip> AnimationClips { get => _modelProperties.AnimationClips; }
+
+    public bool HasAnimation()
+    {
+        return _modelProperties != null;
+    }
+#endregion
+
+    public AnimatedModelRenderer(string assetName)
+    {
+        this._assetName = assetName;
+    }
+
+    public bool LoadContent(ContentManager content)
+    {
+        bool success = false;
+        this._model = content.Load<Model>(_assetName);
+        _modelProperties = _model.Tag as ModelProperties;
+
+        if (_modelProperties != null)
+        {
+            ObtainBones();
+
+            _boneTransforms = new Matrix[_bones.Count];
+
+            _skeleton = new Matrix[_modelProperties.Skeleton.Count];
+            success = true;
+        }
+
+        VertexElement[] test = _model.Meshes[0].MeshParts[0].VertexBuffer.VertexDeclaration.GetVertexElements();
+
+        for (int index = 0; index < test.Length; index++)
+        {
+            var t = test[index];
+            if (t.VertexElementUsage == VertexElementUsage.BlendIndices)
+            {
+                _hasSkinnedVertexType = true;
+            }
+            else if (t.VertexElementUsage == VertexElementUsage.Normal)
+            {
+                _hasNormals = true;
+            }
+            else if (t.VertexElementUsage == VertexElementUsage.TextureCoordinate)
+            {
+                _hasTexCoords = true;
+            }
+        }
+
+        return success;
+    }
+
+    private void ObtainBones()
+    {
+        _bones.Clear();
+
+        foreach (ModelBone bone in _model.Bones)
+        {
+            Bone newBone = new Bone(bone.Name, bone.Transform, bone.Parent != null ? _bones[bone.Parent.Index] : null);
+
+            _bones.Add(newBone);
+        }
+    }
+
+    public Bone FindBone(string name)
+    {
+        foreach (Bone bone in Bones)
+        {
+            if (bone.Name == name)
+            {
+                return bone;
+            }
+        }
+
+        return null;
+    }
+
+#region Animation Management
+    public AnimationPlayer PlayClip(AnimationClip clip, bool loop = true, int keyframeStart = 0, int keyframeEnd = 0, int fps = 24)
+    {
+        _animationPlayer = new AnimationPlayer(clip, this, loop, keyframeStart, keyframeEnd, fps);
+        return _animationPlayer;
+    }
+#endregion
+
+#region Updating
+    public void Update()
+    {
+        _animationPlayer?.Update();
+    }
+#endregion
+
+#region Drawing
+    public void Draw()
+    {
+        
+    }
+#endregion
+}
