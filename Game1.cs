1765088e8ec22e31ca7ca0ac56ae78bd8049448a
242500 <126494791+bazyl02@users.noreply.github.com>
2024-05-14 20:55:21 +0200

    Added back missing includes

--- Game1.cs
+++ Game1.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using ImGuiNET;
--- ModelData.cs
+++ ModelData.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using System.Xml.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
