531e868d7303f4ef5eb9e267964f71bd38cb15ee
Grzegorz Bednarek <gregorooooo02@gmail.com>
2024-05-29 23:46:16 +0200

    Changing content and adding fixed models

--- Game1.cs
+++ Game1.cs
@@ -104,9 +104,10 @@ public class Game1 : Game
         Globals.TerrainEffect = new Effect(_graphics.GraphicsDevice, bytecode);
 #endif
 #if DEBUG
-        _sceneManager.AddScene(new MapScene());
-        _sceneManager.AddScene(FileManager.PopulateScene("Menu"));
+        //_sceneManager.AddScene(new MapScene());
         _sceneManager.AddScene(FileManager.PopulateScene("BaseScene"));
+        _sceneManager.AddScene(FileManager.PopulateScene("Menu"));
+        // _sceneManager.AddScene(FileManager.PopulateScene("BaseScene"));
         //_sceneManager.AddScene(new MapScene());
 #elif RELEASE
         _sceneManager.AddScene(FileManager.PopulateScene("Menu"));
